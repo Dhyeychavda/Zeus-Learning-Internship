cef47261a08ae7d236a88a7cffbd966f3ab48ef2
Dhyey Chavda <dhyeychavda5261@gmail.com>
2023-02-17 10:06:47 +0530

    initial commit weather app

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,47 @@
+class Forecast{
+  constructor(){
+    this.key = 'ACIeRvcPS5dTxbGgAu1bA6TtzgxXimjN';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  }
+  async updateCity(city){
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+    return { cityDets, weather };
+  }
+  async getCity(city){
+
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI+ query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getWeather(id){
+    const query = `${id}?apikey=${this.key}`;
+
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+
+    return data[0];
+
+  }
+}
+
+
+// const key = 'ACIeRvcPS5dTxbGgAu1bA6TtzgxXimjN';
+
+
+// // get weather information
+// const getWeather = async (id) => {
+  
+//   const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  
+// };
+
+// // get city information
+// const getCity = async (city) => {
+
+//   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  
+
+// };
